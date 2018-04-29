2a9dc6d1ccd3bce941e151a9f79e1338ded90591
Hata <ayumu.hatakeyama@gmail.com>
2018-04-29 23:52:15 +0700

    本番DBの情報を設定

--- src/app.php
+++ src/app.php
@@ -20,23 +20,42 @@ $MODE = "debug";
 $app->register(new MonologServiceProvider(), array(
   'monolog.logfile' => 'php://stdout',
   // "monolog.logfile" => ROOT_PATH . "/storage/logs/" . Carbon::now('Europe/London')->format("Y-m-d") . ".log",
-  "monolog.level" => 'debug',//$app["log.level"],
+  "monolog.level" => $MODE, //$app["log.level"],
   "monolog.name" => "application"
 ));
 
-// Heroku DBへの参照
-$app->register(new Csanquer\Silex\PdoServiceProvider\Provider\PDOServiceProvider('pdo'),
-               array(
-                'pdo.server' => array(
-                   'driver'   => 'pgsql',
-                   'user' => "ppbprdbespopyj",
-                   'password' => "b36627a1901b0da76f45c9d4de7184bd464ec43bab90d57de2d951b45233378e",
-                   'host' => "ec2-54-235-109-37.compute-1.amazonaws.com",
-                   'port' => 5432,
-                   'dbname' => ltrim("dclmcej3udp26l",'/')
-                   )
-               )
-);
+// Choice DB connection
+if ($MODE !== 'debug') {
+  // Heroku DBへの参照
+  $app->register(new Csanquer\Silex\PdoServiceProvider\Provider\PDOServiceProvider('pdo'),
+                 array(
+                  'pdo.server' => array(
+                     // Develop
+                     'driver'   => 'pgsql',
+                     'user' => "ppbprdbespopyj",
+                     'password' => "b36627a1901b0da76f45c9d4de7184bd464ec43bab90d57de2d951b45233378e",
+                     'host' => "ec2-54-235-109-37.compute-1.amazonaws.com",
+                     'port' => 5432,
+                     'dbname' => ltrim("dclmcej3udp26l",'/')
+                     )
+                 )
+  );
+} else {
+  // Heroku DBへの参照
+  $app->register(new Csanquer\Silex\PdoServiceProvider\Provider\PDOServiceProvider('pdo'),
+                 array(
+                  'pdo.server' => array(
+                    // Product
+                     'driver'   => 'pgsql',
+                     'user' => "cmpmsbirdyehfw",
+                     'password' => "3f9362b93dc525450c3a635175a1884618e05f73a4431cab0cc5b9237bd855a9",
+                     'host' => "ec2-54-243-54-6.compute-1.amazonaws.com",
+                     'port' => 5432,
+                     'dbname' => ltrim("d188t54hklh0e5",'/')
+                     )
+                 )
+  );
+}
 
 // check auth token
 if ($MODE !== 'debug') {
@@ -93,10 +112,6 @@ $app->before(function (Request $request) {
 $app->register(new \Euskadi31\Silex\Provider\CorsServiceProvider);
 
 $app->register(new ServiceControllerServiceProvider());
-
-// $app->register(new DoctrineServiceProvider(), array(
-//   "db.options" => $app["db.options"]
-// ));
 
 $app->register(new HttpCacheServiceProvider(), array("http_cache.cache_dir" => ROOT_PATH . "/storage/cache",));
 
