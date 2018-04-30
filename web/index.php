c5ffbd607635c87acfc97dae002b3c58c82d3159
Hata <ayumu.hatakeyama@gmail.com>
2018-04-30 09:51:46 +0700

    ホストにより環境設定ファイルの切り替えを実装

--- web/index.php
+++ web/index.php
@@ -6,7 +6,13 @@ define("ROOT_PATH", __DIR__ . "/..");
 
 $app = new Silex\Application();
 
-require __DIR__ . '/../resources/config/dev.php';
+$HTTP_HOST = $_SERVER['HTTP_HOST'];
+if($HTTP_HOST === "dev-homework-api.herokuapp.com"){
+  require __DIR__ . '/../resources/config/dev.php';
+} else if($HTTP_HOST === "homework-api.herokuapp.com"){
+  require __DIR__ . '/../resources/config/prod.php';
+} else {  
+}
 
 require __DIR__ . '/../src/app.php';
 
